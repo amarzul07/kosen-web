775f790ea431d50003ae0f317312563ecad4b945
Gantumur Amarzul <agantumu@yahoo-corp.jp>
2019-05-16 18:19:50 +0900

    Add materialize css platform

--- client/src/TopNav.js
+++ client/src/TopNav.js
@@ -4,17 +4,29 @@ import student from "./images/icon/student.png";
 import school from "./images/icon/school.png";
 import about from "./images/icon/about.png";
 
-
 export default class TopNav extends React.Component {
     render() {
         return (
-            <div className="topnav">
-                <a className="active" href="/"><img src={home} alt="home"/></a>
-                <a href="/news"><img src={about}/></a>
-                <a href="/students"><img src={student}/></a>
-                <a href="/colleges"><img src={school}/></a>
-                <a href="/about"><img src={about}/></a>
-                <div className="title"><span>Kosen Community</span></div>
+            <div>
+                <nav>
+                    <div className="nav-wrapper  teal lighten-2">
+                        <a href="#" className="brand-logo center">
+                            <i className="material-icons">people_outline</i>
+                            Kosen Community
+                        </a>
+                        <ul id="nav-mobile" className="left hide-on-med-and-down">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/students">Students</a></li>
+                            <li><a href="/colleges">Colleges</a></li>
+                            <li><a href="/colleges">News</a></li>
+                            <li><a href="/about">About</a></li>
+                        </ul>
+                        <ul className="right hide-on-med-and-down">
+                            <li><a href="#"><i className="material-icons">search</i></a></li>
+                            <li><a href="#"><i className="material-icons">account_circle</i></a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         )
     }
--- client/src/component/HomePage.js
+++ client/src/component/HomePage.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import TopNav from "../TopNav";
+import M from "materialize-css";
 
 export default class HomePage extends React.Component {
+    componentDidMount() {
+        M.AutoInit();
+
+        let carousel = document.querySelectorAll('.carousel.carousel-slider');
+
+        M.Carousel.init(carousel,{
+            indicators: true,
+            fullWidth: true
+        });
+    }
+
     render() {
         return (
             <div>
                 <TopNav/>
-                Home
+                <div className="carousel carousel-slider center">
+                    <div className="carousel-fixed-item center">
+                        <a className="btn waves-effect white grey-text darken-text-2">button</a>
+                    </div>
+                    <div className="carousel-item red white-text" href="#one!">
+                        <h2>First Panel</h2>
+                        <p className="white-text">This is your first panel</p>
+                    </div>
+                    <div className="carousel-item amber white-text" href="#two!">
+                        <h2>Second Panel</h2>
+                        <p className="white-text">This is your second panel</p>
+                    </div>
+                    <div className="carousel-item green white-text" href="#three!">
+                        <h2>Third Panel</h2>
+                        <p className="white-text">This is your third panel</p>
+                    </div>
+                    <div className="carousel-item blue white-text" href="#four!">
+                        <h2>Fourth Panel</h2>
+                        <p className="white-text">This is your fourth panel</p>
+                    </div>
+                </div>
             </div>
         )
     }
